e6c37ec27dcc2816bd675c3f0abec1c40cf97d11
Jatin Yadav <Jatin.yadav1307@gmail.com>
2015-11-21 12:56:01 +0530

    Add health

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151121051810) do
+ActiveRecord::Schema.define(version: 20151121071557) do
 
   create_table "tags", force: :cascade do |t|
     t.string   "name"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20151121051810) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "authentication_token"
+    t.integer  "health"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token"
