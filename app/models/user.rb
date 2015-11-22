33adceafcb0c04a9b1cd2bb92623db477384ccf5
Jatin Yadav <Jatin.yadav1307@gmail.com>
2015-11-22 23:58:48 +0530

    Added remove Record on User deletion

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,14 @@ class User < ActiveRecord::Base
   has_many :notes
   has_one :record
   after_create :init
+  before_destroy :removeRecord
 
   def init
     Record.create(:health => 50, :experience => 0, :level => 0, :user_id => self.id)
+  end
+
+  def removeRecord
+    Record.find(User.find(self.id).record.id).delete
   end
   acts_as_token_authenticatable
   devise :database_authenticatable, :registerable,
