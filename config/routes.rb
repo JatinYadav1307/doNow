0405d3ce361a0f6d3cc8709e3260fcc4daeaab71
Jatin Yadav <Jatin.yadav1307@gmail.com>
2015-11-24 00:44:06 +0530

    Remove Clutter HTTP

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  match '(:anything)' => 'application#nothing', via: [:options]
+  # match '(:anything)' => 'application#nothing', via: [:options]
   resources :notes
   resources :todos
   resources :habits
