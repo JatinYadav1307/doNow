168dcbf1b2c21c8abdf02027e5bb6210c3fcca5a
Jatin Yadav <Jatin.yadav1307@gmail.com>
2015-11-28 22:20:26 +0530

    Fix records params

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -30,6 +30,6 @@ class RecordsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def record_params
-      params.require(:record).permit(:health, :experience, :level)
+      params.require(:record).permit(:health, :experience, :level, :silver, :gold, :diamonds)
     end
 end
